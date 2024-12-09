1135b8cebdd9f65e7e17fc4a0a2a8fbb98c0a054
ericKwon95 <ericyong95@gmail.com>
2024-12-09 17:37:48 +0900

    feat: 연락처 모델 구현

---
+++ PokeContact/PokeContact/Feature/Model/Contact.swift
@@ -0,0 +1,45 @@
+//
+//  Contact.swift
+//  PokeContact
+//
+//  Created by 권승용 on 12/9/24.
+//
+
+struct Contact {
+    let profileImageURL: String
+    let name: String
+    let phoneNumber: String
+    
+    static let dummies = [
+        Contact(
+            profileImageURL: "",
+            name: "contactName",
+            phoneNumber: "010-1234-5678"
+        ),
+        Contact(
+            profileImageURL: "",
+            name: "contactName",
+            phoneNumber: "010-1234-5678"
+        ),
+        Contact(
+            profileImageURL: "",
+            name: "contactName",
+            phoneNumber: "010-1234-5678"
+        ),
+        Contact(
+            profileImageURL: "",
+            name: "contactName",
+            phoneNumber: "010-1234-5678"
+        ),
+        Contact(
+            profileImageURL: "",
+            name: "contactName",
+            phoneNumber: "010-1234-5678"
+        ),
+        Contact(
+            profileImageURL: "",
+            name: "contactName",
+            phoneNumber: "010-1234-5678"
+        )
+    ]
+}
