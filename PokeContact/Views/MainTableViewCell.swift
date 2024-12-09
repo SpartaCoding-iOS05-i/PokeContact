86fe1e1d8ec281a95351257b870c55bdc7387eb1
zzangmmz <leemj036@gmail.com>
2024-12-09 14:47:16 +0900

    [feat] 프로필 이미지뷰 컴포넌트 profileImageView 생성

--- PokeContact/Views/MainTableViewCell.swift
+++ PokeContact/Views/MainTableViewCell.swift
@@ -7,5 +7,10 @@
 import UIKit
 
 final class MainTableViewCell: UITableViewCell {
-    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.borderWidth = 10
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        return imageView
+    }()
 }
