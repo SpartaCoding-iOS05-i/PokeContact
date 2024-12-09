2e0f6e3a42605d99032a10cf6319fa9b1844e0b5
zzangmmz <leemj036@gmail.com>
2024-12-10 01:46:40 +0900

    [feat] 컴포넌트 오토레이아웃 조정

--- PokeContact/Views/AddMemberView.swift
+++ PokeContact/Views/AddMemberView.swift
@@ -10,7 +10,7 @@ import Alamofire
 final class AddMemberView: UIView {
     private let profileImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.layer.borderWidth = 10
+        imageView.layer.borderWidth = 5
         imageView.layer.borderColor = UIColor.lightGray.cgColor
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
@@ -22,13 +22,12 @@ final class AddMemberView: UIView {
         button.setTitle("랜덤 이미지 생성", for: .normal)
         button.setTitleColor(.lightGray, for: .normal)
         button.backgroundColor = .clear
-        button.titleLabel?.font = .systemFont(ofSize: 10)
+        button.titleLabel?.font = .systemFont(ofSize: 15)
         return button
     }()
     
     private let nameTextField: UITextField = {
         let textField = UITextField()
-        textField.frame.size.height = 30
         textField.borderStyle = .roundedRect
         textField.autocorrectionType = .no
         textField.spellCheckingType = .no
@@ -40,7 +39,6 @@ final class AddMemberView: UIView {
     
     private let phoneNumberTextField: UITextField = {
         let textField = UITextField()
-        textField.frame.size.height = 30
         textField.borderStyle = .roundedRect
         textField.autocorrectionType = .no
         textField.spellCheckingType = .no
@@ -54,8 +52,7 @@ final class AddMemberView: UIView {
         let stackView = UIStackView(arrangedSubviews: [profileImageView, randomImageButton, nameTextField, phoneNumberTextField])
         stackView.axis = .vertical
         stackView.spacing = 15
-        stackView.alignment = .fill
-        stackView.distribution = .fillEqually
+        stackView.alignment = .center
         return stackView
     }()
     
@@ -66,11 +63,32 @@ final class AddMemberView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        self.backgroundColor = .white
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func configureUI() {
+        self.backgroundColor = .white
+        self.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            
+            profileImageView.widthAnchor.constraint(equalToConstant: 200),
+            profileImageView.heightAnchor.constraint(equalTo: profileImageView.widthAnchor),
+            
+            nameTextField.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            nameTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            phoneNumberTextField.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            phoneNumberTextField.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
 }
