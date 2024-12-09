ce5632d77e4defa1874cda75982cf27c79feb67e
zzangmmz <leemj036@gmail.com>
2024-12-10 00:57:19 +0900

    [feat] 전화번호 입력 텍스트필드 phoneNumberTextField 생성

--- PokeContact/Views/AddMemberView.swift
+++ PokeContact/Views/AddMemberView.swift
@@ -38,6 +38,18 @@ final class AddMemberView: UIView {
         return textField
     }()
     
+    private let phoneNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.frame.size.height = 30
+        textField.borderStyle = .roundedRect
+        textField.autocorrectionType = .no
+        textField.spellCheckingType = .no
+        textField.autocapitalizationType = .none
+        textField.placeholder = "전화번호"
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
         self.backgroundColor = .white
