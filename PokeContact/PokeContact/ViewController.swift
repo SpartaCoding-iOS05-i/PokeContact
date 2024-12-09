b721cb0d940e7855dd5c58535d1728a3603d88e9
ericKwon95 <ericyong95@gmail.com>
2024-12-09 10:25:40 +0900

    feat: initial commit

---
+++ PokeContact/PokeContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeContact
+//
+//  Created by 권승용 on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
