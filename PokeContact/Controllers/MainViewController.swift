594f779839721f71f7d96dca2447a03d349d484b
zzangmmz <leemj036@gmail.com>
2024-12-09 21:58:16 +0900

    [feat] 네비게이션바 설정 메서드 configureNavigationBar() 생성

--- PokeContact/Controllers/MainViewController.swift
+++ PokeContact/Controllers/MainViewController.swift
@@ -12,8 +12,15 @@ final class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view = MainView()
+        configureNavigationBar()
     }
-
-
+    
+    private func configureNavigationBar() {
+        self.navigationItem.title = "친구 목록"
+        let addMemberButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addMemberButtonTapped))
+    }
+    
+    @objc private func addMemberButtonTapped() {
+    }
 }
 
