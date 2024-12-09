23aa445ff90cd5801df741703859633e21c56421
zzangmmz <leemj036@gmail.com>
2024-12-09 22:24:49 +0900

    [create] 연락처 추가 AddMemberViewController 생성

---
+++ PokeContact/Controllers/AddMemberViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddMemberViewController.swift
+//  PokeContact
+//
+//  Created by 이명지 on 12/9/24.
+//
+import UIKit
+
+final class AddMemberViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func configureNavigationBar() {
+        self.navigationItem.title = "연락처 추가"
+    }
+}
