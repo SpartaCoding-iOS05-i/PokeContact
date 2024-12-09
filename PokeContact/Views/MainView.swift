e22a38aaa6cff1fd0b6965519dd84fc2c904abaf
zzangmmz <leemj036@gmail.com>
2024-12-09 14:09:44 +0900

    [feat] 생성자 오버라이드, UITableViewDataSource, UITableViewDelegate 프로토콜 채택

--- PokeContact/Views/MainView.swift
+++ PokeContact/Views/MainView.swift
@@ -7,5 +7,23 @@
 import UIKit
 
 final class MainView: UIView {
+    let mainTableView = UITableView()
     
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MainView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
