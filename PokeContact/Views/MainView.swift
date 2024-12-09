caa467bc4ce6e4fed889cffcdb9995cef568bf21
zzangmmz <leemj036@gmail.com>
2024-12-10 00:31:14 +0900

    [feat] 테이블뷰 numberOfRowsInSection 메서드, cellForRowAt 메서드, heightForRowAt 메서드 설정

--- PokeContact/Views/MainView.swift
+++ PokeContact/Views/MainView.swift
@@ -5,25 +5,55 @@
 //  Created by 이명지 on 12/9/24.
 //
 import UIKit
+import CoreData
 
 final class MainView: UIView {
-    let mainTableView = UITableView()
+    private let tableView = UITableView()
+    private var contacts = [NSManagedObject]()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
+        self.backgroundColor = .white
+        configureTableView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func configureTableView() {
+        tableView.register(MainTableViewCell.self, forHeaderFooterViewReuseIdentifier: "MainTableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        self.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
 }
 
 extension MainView: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
+        let contact = contacts[indexPath.row]
+        let profileImage = contact.value(forKey: PokeContactBook.Key.profileImage) as? String ?? ""
+        let name = contact.value(forKey: PokeContactBook.Key.name) as? String ?? ""
+        let phoneNumber = contact.value(forKey: PokeContactBook.Key.phoneNumber) as? String ?? ""
+        cell.configureCellData(profileImage: profileImage, name: name, phoneNumber: phoneNumber)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
