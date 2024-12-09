145438951c817477d724dbcdef002b67038890bc
zzangmmz <leemj036@gmail.com>
2024-12-09 11:48:32 +0900

    [create] 프로젝트 생성

---
+++ PokeContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeContact
+//
+//  Created by 이명지 on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
