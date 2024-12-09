56c401143969050003dbb7fb1b7068605122d73c
ericKwon95 <ericyong95@gmail.com>
2024-12-09 17:58:15 +0900

    refactor: 이미지 URL 타입 String->URL로 변경

--- PokeContact/PokeContact/Feature/Model/Contact.swift
+++ PokeContact/PokeContact/Feature/Model/Contact.swift
@@ -4,40 +4,41 @@
 //
 //  Created by 권승용 on 12/9/24.
 //
+import Foundation
 
 struct Contact {
-    let profileImageURL: String
+    let profileImageURL: URL?
     let name: String
     let phoneNumber: String
     
     static let dummies = [
         Contact(
-            profileImageURL: "",
+            profileImageURL: nil,
             name: "contactName",
             phoneNumber: "010-1234-5678"
         ),
         Contact(
-            profileImageURL: "",
+            profileImageURL: nil,
             name: "contactName",
             phoneNumber: "010-1234-5678"
         ),
         Contact(
-            profileImageURL: "",
+            profileImageURL: nil,
             name: "contactName",
             phoneNumber: "010-1234-5678"
         ),
         Contact(
-            profileImageURL: "",
+            profileImageURL: nil,
             name: "contactName",
             phoneNumber: "010-1234-5678"
         ),
         Contact(
-            profileImageURL: "",
+            profileImageURL: nil,
             name: "contactName",
             phoneNumber: "010-1234-5678"
         ),
         Contact(
-            profileImageURL: "",
+            profileImageURL: nil,
             name: "contactName",
             phoneNumber: "010-1234-5678"
         )
