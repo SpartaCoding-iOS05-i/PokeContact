73d8753051b425cbb789f4bf13d7dfdedd6d9d4a
zzangmmz <leemj036@gmail.com>
2024-12-10 13:42:22 +0900

    [feat] NetworkError 정의, 랜덤으로 포켓몬 이미지 받아와 처리하는 fetchRandomPokemon() 메서드 구현

--- PokeContact/Models/PokeDataManager.swift
+++ PokeContact/Models/PokeDataManager.swift
@@ -8,7 +8,35 @@ import UIKit
 import Alamofire
 
 final class PokeDataManager {
-    
+    func fetchRandomPokemon(completion: @escaping (Result<UIImage, Error>) -> Void) {
+            let url = "https://pokeapi.co/api/v2/pokemon/\(Int.random(in: 1...1000))"
+            
+            AF.request(url).responseDecodable(of: PokeResponse.self) { response in
+                switch response.result {
+                case .success(let pokemon):
+                    if let spriteUrl = pokemon.sprites.frontDefault,
+                       let imageUrl = URL(string: spriteUrl) {
+                        
+                        AF.request(imageUrl).responseData { imageResponse in
+                            switch imageResponse.result {
+                            case .success(let imageData):
+                                if let image = UIImage(data: imageData) {
+                                    completion(.success(image))
+                                } else {
+                                    completion(.failure(NetworkError.invalidImageData))
+                                }
+                            case .failure(let error):
+                                completion(.failure(error))
+                            }
+                        }
+                    } else {
+                        completion(.failure(NetworkError.noImageUrl))
+                    }
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
 }
 
 struct PokeResponse: Decodable {
@@ -21,4 +49,9 @@ struct PokeResponse: Decodable {
             case frontDefault = "front_default"
         }
     }
+}
+
+enum NetworkError: Error {
+    case noImageUrl
+    case invalidImageData
 }
