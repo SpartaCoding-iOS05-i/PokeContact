62b7fa8b39f752dd10019e5e9af45bfcd1f2fd35
zzangmmz <leemj036@gmail.com>
2024-12-10 12:58:08 +0900

    [feat] 코어 데이터 Create 구현

--- PokeContact/Controllers/AddMemberViewController.swift
+++ PokeContact/Controllers/AddMemberViewController.swift
@@ -5,12 +5,15 @@
 //  Created by 이명지 on 12/9/24.
 //
 import UIKit
+import CoreData
 
 final class AddMemberViewController: UIViewController {
+    let addMemberView = AddMemberView()
+    var container: NSPersistentContainer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = AddMemberView()
+        view = addMemberView
         configureNavigationBar()
     }
     
@@ -21,6 +24,37 @@ final class AddMemberViewController: UIViewController {
     }
     
     @objc private func completeButtonTapped() {
+        let image = addMemberView.profileImageView.image?.toString() ?? ""
+        let name = addMemberView.nameTextField.text ?? ""
+        let phoneNumber = addMemberView.phoneNumberTextField.text ?? ""
         
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        self.container = appDelegate.persistentContainer
+        createMember(profileImage: image, name: name, phoneNumber: phoneNumber)
+    }
+    
+    private func createMember(profileImage: String, name: String, phoneNumber: String) {
+        guard let entity = NSEntityDescription.entity(forEntityName: PokeContactBook.className, in: self.container.viewContext) else { return }
+        
+        let newPoke = NSManagedObject(entity: entity, insertInto: self.container.viewContext)
+        newPoke.setValue(profileImage, forKey: PokeContactBook.Key.profileImage)
+        newPoke.setValue(name, forKey: PokeContactBook.Key.name)
+        newPoke.setValue(phoneNumber, forKey: PokeContactBook.Key.phoneNumber)
+        
+        do {
+            try self.container.viewContext.save()
+            print("context save 성공")
+        } catch {
+            print("context svae 실패")
+        }
+    }
+}
+
+extension UIImage {
+    func toString() -> String? {
+        guard let imageData = self.pngData() else {
+            return nil
+        }
+        return imageData.base64EncodedString()
     }
 }
