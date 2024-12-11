096a8a30bd3d08ee7d3d8149d1d1fe8285a17923
zzangmmz <leemj036@gmail.com>
2024-12-11 18:46:56 +0900

    [feat] 사용자 입력값 유효성 에러 ValidationError enum 정의

--- PokeContact/Models/InputValidator.swift
+++ PokeContact/Models/InputValidator.swift
@@ -6,6 +6,23 @@
 //
 import UIKit
 
+enum ValidationError: LocalizedError {
+    case emptyName
+    case emptyPhoneNumber
+    case invalidPhoneNumber
+    
+    var errorMessage: String {
+        switch self {
+        case .emptyName:
+            return "이름을 입력해주세요"
+        case .emptyPhoneNumber:
+            return "전화번호를 입력해주세요"
+        case .invalidPhoneNumber:
+            return "올바른 전화번호를 입력해주세요"
+        }
+    }
+}
+
 final class InputValidator {
     
 }
