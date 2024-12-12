6951322e1e6d6a130ca49ce01059088567fad9e8
zzangmmz <leemj036@gmail.com>
2024-12-12 12:48:43 +0900

    [refactor] NSManagedObject -> Contact 인스턴스 생성해주는 메서드 구현

--- PokeContact/Models/Contact.swift
+++ PokeContact/Models/Contact.swift
@@ -5,8 +5,16 @@
 //  Created by 이명지 on 12/12/24.
 //
 
+import CoreData
+
 struct Contact {
     let name: String
     let phoneNumber: String
     let profileImage: String
+    
+    init(_ object: NSManagedObject) {
+        self.name = object.value(forKey: PokeContactBook.Key.name) as? String ?? ""
+        self.phoneNumber = object.value(forKey: PokeContactBook.Key.phoneNumber) as? String ?? ""
+        self.profileImage = object.value(forKey: PokeContactBook.Key.profileImage) as? String ?? ""
+    }
 }
